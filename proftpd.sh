0b647584c21b56996d691e3bdc5efe1149b4f27f
PT. Tujuh Ion Indonesia <37320593+tujuhion@users.noreply.github.com>
2018-08-04 05:42:02 +0700

    Update proftpd.sh

--- proftpd.sh
+++ proftpd.sh
@@ -1,5 +1,6 @@
 rpm -iUvh http://dl.fedoraproject.org/pub/epel/7/x86_64/e/epel-release-7-5.noarch.rpm
 yum -y install proftpd
+sed -i "s/ProFTPD server/$HOSTNAME/g" /etc/proftpd.conf
 systemctl restart proftpd
 systemctl enable proftpd
 firewall-cmd --permanent --add-port=21/tcp
