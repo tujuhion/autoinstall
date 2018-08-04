d91020ec7b846b9b2d3556cbbb307fccffed4702
PT. Tujuh Ion Indonesia <37320593+tujuhion@users.noreply.github.com>
2018-08-04 14:48:21 +0700

    Update proftpd.sh

--- proftpd.sh
+++ proftpd.sh
@@ -1,4 +1,4 @@
-rpm -iUvh http://dl.fedoraproject.org/pub/epel/7/x86_64/e/epel-release-7-5.noarch.rpm
+yum -y install epel-release
 yum -y install proftpd
 sed -i "s/ProFTPD server/$HOSTNAME/g" /etc/proftpd.conf
 systemctl restart proftpd
