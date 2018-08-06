3d024e382d8b173035f3e9b353ab741ba65daf72
PT. Tujuh Ion Indonesia <37320593+tujuhion@users.noreply.github.com>
2018-08-07 04:50:03 +0700

    Update rancher.sh

--- rancher.sh
+++ rancher.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 yum remove docker docker-common docker-selinux docker-engine
 yum install -y yum-utils device-mapper-persistent-data lvm2
 yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
